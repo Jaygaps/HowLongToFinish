e5945df79f3f5f092f576373965cdf9141f03219
jaygaps <jayrajsurti@hotmail.com>
2022-06-13 15:59:26 +1200

    added all development

--- journey/Home/Home.tsx
+++ journey/Home/Home.tsx
@@ -4,6 +4,7 @@ import styled, { css } from "styled-components";
 import axios from "axios";
 
 import Layout from "../../components/Layout";
+import Link from "next/link";
 
 const Container = styled.div`
   height: 100vh;
@@ -19,6 +20,13 @@ const Header = styled.div`
   margin: 20px 0 60px 0;
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
+
+  ${({ theme: { media } }) => css`
+    ${media.up("tablet")} {
+      flex-direction: row;
+    }
+  `};
 `;
 
 const Title = styled.h2``;
@@ -45,11 +53,11 @@ const SmallParagraph = styled.p`
 
 const EpisodeInput = styled.input`
   border: 3px solid ${({ theme: { colors } }) => colors.secondary};
+  margin: 0 16px;
   display: inline-block;
   background: transparent;
   border-radius: 4px;
   padding: 20px;
-  margin: 0 16px;
   color: ${({ theme: { colors } }) => colors.secondary};
   font-size: 24px;
   max-width: 100px;
@@ -68,15 +76,36 @@ const MainInput = styled.input`
   padding: 20px;
   color: ${({ theme: { colors } }) => colors.secondary};
   font-size: 24px;
+  margin-bottom: 20px;
 `;
 
-const TypeAheadWrapper = styled.div``;
+const TypeAheadWrapper = styled.div`
+  ${({ theme: { media } }) => css`
+    display: grid;
+    grid-template-columns: 1fr;
+
+    ${media.up("tablet")} {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    ${media.up("desktop")} {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+  `};
+  gap: 20px;
+`;
 
 const TypeAheadCard = styled.div`
   background-color: ${({ theme: { colors } }) => colors.primary};
   box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%);
-  padding: 30px;
-  border-bottom: 1px solid white;
+  border-radius: 20px;
+  position: relative;
+
+  border: 1px solid #222;
+
+  > img {
+    border-radius: 20px;
+  }
 
   cursor: pointer;
   &:hover {
@@ -141,17 +170,59 @@ const MobileImage = styled.img`
 
 const Dev = styled.p`
   color: grey;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:hover {
+    color: #f8f8f8;
+  }
+`;
+
+const Popularity = styled.div`
+  position: absolute;
+  right: 0;
+  background-color: white;
+  padding: 10px;
+  border-radius: 20px;
+  color: black;
+  font-weight: bold;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+`;
+
+const PlaceholderImage = styled.div`
+  width: 100%;
+  min-height: 750px;
+  background-color: white;
+  border-radius: 20px;
+  min-height: 296px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+`;
+
+const CardContent = styled.div`
+  padding: 16px;
 `;
 
 const Home: NextPage = () => {
   const [episode, setEpisode] = useState(1);
   const [showName, setShowName] = useState("");
   const [typeAheadShows, setTypeAheadShows] = useState([]);
-  const [selectedShow, setSelectedShow] = useState({});
+  const [selectedShow, setSelectedShow] = useState<any>({});
 
   const handleOnChangeShow = (e: any) => {
+    if (e.target.value === "") {
+      setShowName("");
+      setSelectedShow({});
+
+      return;
+    }
     setSelectedShow({});
     setShowName(e.target.value);
+
     axios
       .get(
         `https://api.themoviedb.org/3/search/tv?api_key=54e8661cf9f6d1d987d2cc01a5a1e2d8&language=en-US&query=${e.target.value}`
@@ -169,7 +240,7 @@ const Home: NextPage = () => {
 
   const imgSrc = `https://image.tmdb.org/t/p/w500${selectedShow.backdrop_path}`;
 
-  const daysToCompleteShow = selectedShow.number_of_episodes / episode;
+  const daysToCompleteShow = selectedShow.number_of_episodes / episode ?? 1;
 
   function timeConvert(n: number) {
     const num = n;
@@ -184,8 +255,13 @@ const Home: NextPage = () => {
     <Layout>
       <Container>
         <Header>
-          <Title>How Long To Finish</Title>
-          <Dev>Dev by JayGaps</Dev>
+          <Title>How Long To Binge</Title>
+          <div>
+            <Link href="https://www.linkedin.com/in/jayraj-surti-b736bb138/">
+              <Dev>Dev by JayGaps</Dev>
+            </Link>
+            <label>ZINC Development</label>
+          </div>
         </Header>
 
         <FlexWrapper>
@@ -195,7 +271,7 @@ const Home: NextPage = () => {
             value={episode}
             onChange={(e) => setEpisode(e.target.value)}
           />
-          <Paragraph>episodes per day...</Paragraph>
+          <Paragraph>episode(s) per day...</Paragraph>
         </FlexWrapper>
 
         <MainInput
@@ -246,12 +322,28 @@ const Home: NextPage = () => {
         ) : (
           <TypeAheadWrapper>
             {typeAheadShows && typeAheadShows !== undefined
-              ? typeAheadShows.map((show, index) => (
+              ? typeAheadShows.map((show: any, index) => (
                   <TypeAheadCard
                     key={index}
                     onClick={() => handleSelectedShow(show.id)}
                   >
-                    {show.name}
+                    <Popularity>{show.vote_average * 10}%</Popularity>
+                    {show.poster_path ? (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
+                        width="100%"
+                      />
+                    ) : show.backdrop_path ? (
+                      <img
+                        width="100%"
+                        src={`https://image.tmdb.org/t/p/w500${show.backdrop_path}`}
+                      />
+                    ) : (
+                      <PlaceholderImage>No image</PlaceholderImage>
+                    )}
+                    <CardContent>
+                      <h3>{show.name}</h3>
+                    </CardContent>
                   </TypeAheadCard>
                 ))
               : null}
